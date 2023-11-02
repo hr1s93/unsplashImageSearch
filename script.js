1f686454a87af93c0904a4b742b5ea3049cc81ae
Hristiyan Hristov <142612927+hr1s93@users.noreply.github.com>
2023-11-02 13:33:03 +0100

    uploading the files

---
+++ script.js
@@ -0,0 +1,58 @@
+const accessKey = "VMn27xDstjOND6WUUJSZcc4rr9aaI7nwZ_RoiiaGqEU";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("input-search");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("btn-show-more");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.forEach((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+  page++;
+
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+
+  if (searchResults.innerHTML === "") {
+    showMore.style.display = "none";
+  }
+}
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click", () => {
+  searchImages();
+});
